bc7f5a2a5d3341301fb6adab1e4a185a864e98e6
Angus Scroggie <ascroggie03@gmail.com>
2025-01-12 23:22:01 +1000

    Create main timesheet dataframe

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from timeformat import TrackTime
 
 sns.set_theme(style="whitegrid")
 
+STANDARDS_150 = pd.read_csv("data/150cc_standards.csv")
+STANDARDS_200 = None
+
 def raw_to_csv(input_filename: str, output_filename: str):
     """Converts a text file with track times to CSV. Text file format should be tab separated, such
     as `track_name \\t track_time`.
@@ -40,39 +43,119 @@ def clean_standards(filename: str):
     df.drop(["Cup", "WR", "-"], axis=1, inplace=True)
     df.to_csv(filename, index=False)
 
-def basic_analysis(data: DataFrame):
+def basic_analysis(data: DataFrame, verbose: bool = False):
     """Runs a basic analysis on the given df, printing the results.
 
     Args:
-        data (DataFrame): df of interest.
+        data (DataFrame): df of interest
+        verbose (bool): if true, prints more information about the df
     """
     print("First and last 5 rows:")
     print(data.head())
+    print()
     print(data.tail())
     
     print("\nShape (rows, columns):")
     print(data.shape)
     
-    # Display information about the dataset (columns, non-null values, dtypes)
-    print("\nInfo about the dataset:")
-    print(data.info())
-    
-    print("\nMissing values:")
-    print(data.isnull().sum())
+    if verbose:
+        # Display information about the dataset (columns, non-null values, dtypes)
+        print("\nInfo about the dataset:")
+        print(data.info())
+        
+        print("\nMissing values:")
+        print(data.isnull().sum())
 
-    print("\nDuplicates info:")
-    dupes = data[data.duplicated()]
-    print(f"Count: {dupes.shape}")
-    print(dupes)
-    
-    print("\nDescriptive statistics:")
-    print(data.describe())
+        # print("\nDuplicates info:")
+        # dupes = data[data.duplicated()]
+        # print(f"Count: {dupes.shape}")
+        # print(dupes)
+        
+        print("\nDescriptive statistics:")
+        print(data.describe())
 
-def create_timesheet_df(pbs: DataFrame, wrs: DataFrame, standards: DataFrame):
-    pass
+def determine_standard_and_diff(track_no: int, time: TrackTime, 
+    standards: DataFrame | None = None) -> tuple[str, TrackTime]:
+    """Calculate which Standard (rank) a given time falls in based on the given cut-off times. For
+    Mario Kart, standards are usually something like:
+    `God > Myth > Titan > Hero > Exp > Adv > Int > Beg`
+    with subdivisions like `A > B > C`.
+
+    Args:
+        track_no (int): the track number for the given time.
+        time (TrackTime): the given time to categorise.
+        standards (DataFrame, optional): the cut-offs to determine the rank, defaults to default
+        150cc standards.
+
+    Returns:
+        tuple[str, TrackTime]: tuple containing the rank name and the difference to the next rank
+    """
+    if standards is None: # Check if using custom standards
+        standards = STANDARDS_150
+
+    row = standards.iloc[track_no]
+    for i, item in enumerate(row[1:].items()):
+        rank, cutoff = item
+        if time <= cutoff:
+            # Get the previous bound if not already top rank
+            to_next = row.get(i) if i > 0 else None
+            return (rank, time - TrackTime(to_next)) if to_next else (rank, TrackTime("0:00.000"))
+    return "Unranked", time - TrackTime(row.iloc[-1])
+
+def create_timesheet_df(pbs: DataFrame, wrs: DataFrame, standards: DataFrame | None = None) -> DataFrame:
+    """Create a timesheet with various data based on the given times and WRs. Standards can be 
+    optionally included if desired, otherwise data will be only based on WR comparisons.
+    
+    Timesheet will contain:
+    - TrackNo: track number (1-96)
+    - Track: track name
+    - Time: given user time
+    - Standard: rank name based on the given standard cut-offs
+    - StandardDiff: difference between the next standard and the user time
+    - WR: given world record
+    - Diff: time - wr
+    - DiffNormWR: diff / wr * 100
+    - DiffNormPB: diff / time * 100
+    - [Name]Num: numerical version of TrackTime columns for plotting purposes
+
+    Args:
+        pbs (DataFrame): Some given times.
+        wrs (DataFrame): WR times.
+        standards (DataFrame, optional): Standards by which to categorise the given times. Defaults 
+        to None.
+
+    Returns:
+        DataFrame: Filled timesheet.
+    """
+    col_names = ["TrackNo", "TrackName", "Time", "Standard", "StandardDiff", "WR", 
+                 "WRDiff", "WRDiffNormWR", "WRDiffNormPB",
+                 "TimeNum", "StandardDiffNum", "WRNum", "WRDiffNum"]
+    timesheet = []
+    for track_no in range(len(pbs)):
+        name, time = pbs.iloc[track_no]
+        wr = wrs.iloc[track_no, 1]
+
+        # Convert to TrackTime class to handle operations
+        time = TrackTime(time)
+        wr = TrackTime(wr)
+        standard, stnd_diff = determine_standard_and_diff(track_no, time)
+        wr_diff = time - wr
+
+        timesheet.append([
+            track_no + 1, name, time, standard, stnd_diff, wr, wr_diff, 
+            wr_diff.get_seconds() / wr.get_seconds() * 100, wr_diff.get_seconds() / time.get_seconds() * 100,
+            time.get_seconds(), stnd_diff.get_seconds(), wr.get_seconds(), wr_diff.get_seconds()
+        ])
+
+    return DataFrame(timesheet, columns=col_names)
 
 if __name__ in "__main__":
-    times_150 = pd.read_csv("150cc_times.csv")
-    wrs_150 = pd.read_csv("150cc_wrs_12_01_2025.csv")
-    standards_150 = pd.read_csv("150cc_standards.csv")
-    timesheet = create_timesheet_df(times_150, wrs_150, standards_150)
+    # Create timesheet
+    times_150 = pd.read_csv("data/150cc_times.csv")
+    wrs_150 = pd.read_csv("data/150cc_wrs_12_01_2025.csv")
+    timesheet = create_timesheet_df(times_150, wrs_150)
+
+    # Do stuff with it
+    basic_analysis(timesheet)
+    sns.histplot(data=timesheet, x="TimeNum")
+    plt.show()
