037f2e59c5d8bb60e19ed87dd156d0be10cb7267
Angus Scroggie <ascroggie03@gmail.com>
2025-02-12 23:05:24 +1000

    Change colormap to d3

--- static/timesheet.js
+++ static/timesheet.js
@@ -1,4 +1,4 @@
-import colormap from "https://cdn.jsdelivr.net/npm/colormap@2.3.2/+esm";
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 document.addEventListener("DOMContentLoaded", function() {
     let table = new DataTable("#timesheetTable", {
@@ -16,15 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Colour Standard col
     document.querySelectorAll(".colour-rank").forEach(cell => {
-        let rank = cell.innerText.trim(); // Get text inside the cell
-        if (rankColormap[rank]) {
-            cell.style.backgroundColor = rankColormap[rank];
+        let rank = cell.innerText.trim();
+        if (rankColormap.has(rank)) {
+            cell.style.backgroundColor = rankColormap.get(rank);
         }
     });
 
     // Colour WR Diff col
     document.querySelectorAll(".colour-diff").forEach(cell => {
-        let diff = parseFloat(cell.getAttribute("data-sort")); // Convert to number
+        let diff = parseFloat(cell.getAttribute("data-sort"));
         cell.style.backgroundColor = getWRDiffColor(diff);
     });
 });
@@ -40,32 +40,21 @@ const standardsNames = [
     'Beg A', 'Beg B', 'Beg C'
 ];
 
-// See: https://www.jsdelivr.com/package/npm/colormap
-const rankColors = colormap({
-    colormap: "plasma",
-    nshades: standardsNames.length,
-    format: "hex",
-    alpha: 1
+// See: https://d3js.org/d3-scale-chromatic
+const rankColormap = new Map();
+standardsNames.forEach((rank, i) => {
+    rankColormap.set(rank, d3.interpolateViridis(i / (standardsNames.length - 1)));
 });
 
-let rankColormap = {};
-for (let i = 0; i < standardsNames.length; i++) {
-    rankColormap[standardsNames[i]] = rankColors[i]
-};
-
-const DIFF_MIN = 0;
 const DIFF_MAX = 10;
-const DIFF_STEP = 2;
-const DIFF_COUNT = Math.floor((DIFF_MAX - DIFF_MIN) / DIFF_STEP) + 1;
-
-const diffColors = colormap({
-    colormap: "bluered",
-    nshades: DIFF_COUNT,
-    format: "hex",
-    alpha: 1
-});
+const DIFF_STEP = 1.0;
 
 function getWRDiffColor(diff) {
-    let index = Math.max(0, Math.min(Math.floor(diff / DIFF_STEP), diffColors.length - 1));
-    return diffColors[index];
+    // Normalise based on diff range
+    // let normalisedDiff = Math.max(DIFF_MIN, Math.min(diff, DIFF_MAX)) / DIFF_MAX;
+    // return d3.interpolateSpectral(1 - normalizedDiff);
+
+    // Set interval mapping
+    let index = Math.max(0, Math.min(Math.floor(diff / DIFF_STEP) / (DIFF_MAX / DIFF_STEP), 1));
+    return d3.interpolateSpectral(1 - index);
 };
