7aa1ed1dd4d4c0d3dbc50c2efc3fc0b55d6a44b3
Angus Scroggie <ascroggie03@gmail.com>
2025-02-13 00:13:58 +1000

    Add subrank colour grouping experiment (better)

--- static/timesheet.js
+++ static/timesheet.js
@@ -43,7 +43,20 @@ const standardsNames = [
 // See: https://d3js.org/d3-scale-chromatic
 const rankColormap = new Map();
 standardsNames.forEach((rank, i) => {
-    rankColormap.set(rank, d3.interpolateViridis(i / (standardsNames.length - 1)));
+    // Evenly spaced color for each standard
+    // rankColormap.set(rank, d3.interpolateTurbo(i / (standardsNames.length - 1)));
+
+    // Some bullshit to group subranks closer together using tint
+    let index = 0.05;
+    let color = tintColor(d3.interpolateTurbo(index), 30);
+    if (i > 0) {
+        index += Math.floor((i+2)/3) * 0.9 / 7;
+        color = d3.interpolateTurbo(index);
+        if ((i-1) % 3 == 0) { color = tintColor(color, 10, true); }
+        if ((i-1) % 3 == 1) { color = tintColor(color, 30); }
+        if ((i-1) % 3 == 2) { color = tintColor(color, 50); }
+    };
+    rankColormap.set(rank, color);
 });
 
 const DIFF_MAX = 10;
@@ -57,4 +70,15 @@ function getWRDiffColor(diff) {
     // Set interval mapping
     let index = Math.max(0, Math.min(Math.floor(diff / DIFF_STEP) / (DIFF_MAX / DIFF_STEP), 1));
     return d3.interpolateSpectral(1 - index);
+};
+
+function tintColor(color, percent, darken = false) {
+    let rgb = d3.color(color); // d3.color() parses any valid color
+    if (!rgb) return color;
+
+    // Lightens the given colour by default, otherwise darkens
+    let bw = "#FFFFFF";
+    if (darken) { bw = "#000000"; }
+
+    return d3.interpolateRgb(color, bw)(percent / 100);;
 };
