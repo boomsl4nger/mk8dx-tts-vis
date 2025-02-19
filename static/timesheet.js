81508c283783503a6c6802dfffe5ee04ba16b98b
Angus Scroggie <ascroggie03@gmail.com>
2025-02-19 16:13:54 +1000

    Refactor timesheet scripts out to js file

--- static/timesheet.js
+++ static/timesheet.js
@@ -1,34 +1,6 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-document.addEventListener("DOMContentLoaded", function() {
-    let table = new DataTable("#timesheetTable", {
-        // See: https://datatables.net/manual/options
-        paging: false,
-        info: false,
-        searching: false,
-        columnDefs: [
-            {className: "dt-head-left", targets: "_all"},
-            {className: "dt-body-center", targets: [2, 3, 4, 5, 6, 7]},
-            {width: "12%", targets: [2, 3, 4, 5, 6, 7]},
-            // {orderable: false, targets: [0]}
-        ]
-    });
-
-    // Colour Standard col
-    document.querySelectorAll(".colour-rank").forEach(cell => {
-        let rank = cell.innerText.trim();
-        if (rankColormap.has(rank)) {
-            cell.style.backgroundColor = rankColormap.get(rank);
-        }
-    });
-
-    // Colour WR Diff col
-    document.querySelectorAll(".colour-diff").forEach(cell => {
-        let diff = parseFloat(cell.getAttribute("data-sort"));
-        cell.style.backgroundColor = getWRDiffColor(diff);
-    });
-});
-
+// Colouring cells
 const standardsNames = [
     'God', 
     'Myth A', 'Myth B', 'Myth C', 
@@ -84,3 +56,76 @@ function tintColor(color, percent, darken = false) {
 
     return d3.interpolateRgb(color, bw)(percent / 100);;
 };
+
+// Charts
+const wrDiffCtx = document.getElementById('wrDiffChart').getContext('2d');
+const wrDiffData = {
+    labels: diff_labels,
+    datasets: [{
+        label: 'WR Diff Distribution',
+        data: diff_data
+    }]
+};
+
+new Chart(wrDiffCtx, {
+    type: 'bar',
+    data: wrDiffData,
+    options: {
+        responsive: true,
+        aspectRatio: 1.4,
+        scales: {
+            y: { beginAtZero: true }
+        }
+    }
+});
+
+const rankCtx = document.getElementById('rankChart').getContext('2d');
+const rankData = {
+    labels: rank_labels,
+    datasets: [{
+        label: 'Rank Distribution',
+        data: rank_data
+    }]
+};
+
+new Chart(rankCtx, {
+    type: 'bar',
+    data: rankData,
+    options: {
+        responsive: true,
+        aspectRatio: 1.4,
+        indexAxis: 'y', // Vertical bar chart
+        scales: {
+            x: { beginAtZero: true }
+        }
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let table = new DataTable("#timesheetTable", {
+        // See: https://datatables.net/manual/options
+        paging: false,
+        info: false,
+        searching: false,
+        columnDefs: [
+            { className: "dt-head-left", targets: "_all" },
+            { className: "dt-body-center", targets: [2, 3, 4, 5, 6, 7] },
+            { width: "11%", targets: [2, 3, 4, 5, 6, 7] },
+            // { orderable: false, targets: [0] }
+        ]
+    });
+
+    // Colour Standard col
+    document.querySelectorAll(".colour-rank").forEach(cell => {
+        let rank = cell.innerText.trim();
+        if (rankColormap.has(rank)) {
+            cell.style.backgroundColor = rankColormap.get(rank);
+        }
+    });
+
+    // Colour WR Diff col
+    document.querySelectorAll(".colour-diff").forEach(cell => {
+        let diff = parseFloat(cell.getAttribute("data-sort"));
+        cell.style.backgroundColor = getWRDiffColor(diff);
+    });
+});
